0a7cad40142c7a73a497185311a6393d9889087b
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-06-23 10:59:26 -0400

    created package.json file

--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,27 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+              test: /\.jsx?/,
+              exclude: /node_modules/,
+              use: {
+                  loader: 'babel-loader',
+                  options: {
+                      presets: ['@babel/preset-env', '@babel/preset-react']
+                  }
+              }
+            },
+            {
+              test: /\.s[ac]ss$/i,
+              use: [
+                  { loader: 'style-loader' },
+                  { loader: 'css-loader' },
+                  { loader: 'sass-loader' },
+              ]
+            }
+        ]
     }
 }
