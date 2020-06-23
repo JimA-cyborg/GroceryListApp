68213fb2f7c26588da333c0628dcd931f427b24f
JIMvsTHEWORLD <JMArmbruster@gmail.com>
2020-06-23 10:52:14 -0400

    6/23 10:52 am

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,9 @@
 const path = require('path');
+
+module.exports = {
+    entry: './client/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    }
+}
